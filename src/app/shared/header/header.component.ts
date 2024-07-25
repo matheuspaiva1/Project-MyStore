8185d42465e76dbd7134f7612acacd26cf85414b
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-07-25 18:52:15 -0300

    feat: layout do header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatIcon,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  
 }
